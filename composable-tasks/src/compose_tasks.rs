4077c6a6dbd2bd0bb1032c856ce086c47bd65247
Costin Carabaș <costin.carabas@gmail.com>
2023-12-22 16:36:59 +0200

    Composable tasks: Code refactor

--- composable-tasks/src/compose_tasks.rs
+++ composable-tasks/src/compose_tasks.rs
@@ -32,7 +32,7 @@ pub trait TaskCall:
         let mut payment_for_next_task = self.call_value().egld_or_single_esdt();
         let mut payments_to_return = PaymentsVec::new();
 
-        let caller = self.blockchain().get_caller();
+        let mut dest_addr = self.blockchain().get_caller();
 
         for task in tasks.into_iter() {
             let (task_type, args) = task.into_tuple();
@@ -42,84 +42,101 @@ pub trait TaskCall:
             payment_for_next_task = match task_type {
                 TaskType::WrapEGLD => self.wrap_egld(payment_for_current_task),
                 TaskType::UnwrapEGLD => self.unwrap_egld(payment_for_current_task),
-                TaskType::Swap => {
-                    require!(
-                        !payment_for_current_task.token_identifier.is_egld(),
-                        "EGLD can't be swapped!"
-                    );
-                    let payment_in = payment_for_current_task.unwrap_esdt();
-
-                    require!(args.len() == 2, "Swap requires only 2 arguments");
-
-                    let token_out = TokenIdentifier::from(args.get(0).clone_value());
-                    let min_amount_out = BigUint::from(args.get(1).clone_value());
-
-                    self.perform_tokens_swap(
-                        payment_in.token_identifier,
-                        payment_in.amount,
-                        token_out,
-                        min_amount_out,
-                    )
-                    .into()
-                }
+                TaskType::Swap => self.swap(payment_for_current_task, args),
                 TaskType::RouterSwap => {
-                    require!(
-                        !payment_for_current_task.token_identifier.is_egld(),
-                        "EGLD can't be swapped!"
-                    );
-                    require!(
-                        args.len() % 4 == 0,
-                        "Invalid number of router swap arguments"
-                    );
-                    let payment_in = payment_for_current_task.unwrap_esdt();
-                    let mut returned_payments_by_router = self.multi_pair_swap(payment_in, args);
-
-                    let last_payment_index = returned_payments_by_router.len() - 1;
-                    let payment_out = returned_payments_by_router.take(last_payment_index);
-                    payments_to_return.append_vec(returned_payments_by_router);
-                    EgldOrEsdtTokenPayment::from(payment_out)
+                    self.router_swap(payment_for_current_task, &mut payments_to_return, args)
                 }
                 TaskType::SendEgldOrEsdt => {
-                    self.require_min_expected_token(
-                        &min_expected_token_out,
-                        &payment_for_current_task,
-                    );
                     require!(args.len() == 1, "Invalid number of arguments!");
+                    let new_destination =
+                        ManagedAddress::try_from(args.get(0).clone_value()).unwrap_or(dest_addr);
 
-                    let dest_addr =
-                        ManagedAddress::try_from(args.get(0).clone_value()).unwrap_or(caller);
-
-                    if payment_for_current_task.token_identifier.is_egld() {
-                        self.send()
-                            .direct_egld(&dest_addr, &payment_for_current_task.amount);
-                    } else {
-                        payments_to_return.push(EsdtTokenPayment::new(
-                            payment_for_current_task.token_identifier.unwrap_esdt(),
-                            payment_for_current_task.token_nonce,
-                            payment_for_current_task.amount,
-                        ));
-                    }
-                    if !payments_to_return.is_empty() {
-                        self.send().direct_multi(&dest_addr, &payments_to_return);
-                    }
-                    return;
+                    dest_addr = new_destination;
+                    break;
                 }
             };
         }
-        self.require_min_expected_token(&min_expected_token_out, &payment_for_next_task);
+        self.send_resulted_payments(
+            dest_addr,
+            min_expected_token_out,
+            payment_for_next_task,
+            &mut payments_to_return,
+        )
+    }
 
-        if payment_for_next_task.token_identifier.is_egld() {
+    fn swap(
+        &self,
+        payment_for_current_task: EgldOrEsdtTokenPayment,
+        args: ManagedVec<ManagedBuffer>,
+    ) -> EgldOrEsdtTokenPayment {
+        require!(
+            !payment_for_current_task.token_identifier.is_egld(),
+            "EGLD can't be swapped!"
+        );
+        let payment_in = payment_for_current_task.unwrap_esdt();
+
+        require!(args.len() == 2, "Swap requires only 2 arguments");
+
+        let token_out = TokenIdentifier::from(args.get(0).clone_value());
+        let min_amount_out = BigUint::from(args.get(1).clone_value());
+
+        self.perform_tokens_swap(
+            payment_in.token_identifier,
+            payment_in.amount,
+            token_out,
+            min_amount_out,
+        )
+        .into()
+    }
+
+    fn router_swap(
+        &self,
+        payment_for_current_task: EgldOrEsdtTokenPayment,
+        payments_to_return: &mut PaymentsVec<Self::Api>,
+        args: ManagedVec<ManagedBuffer>,
+    ) -> EgldOrEsdtTokenPayment {
+        require!(
+            !payment_for_current_task.token_identifier.is_egld(),
+            "EGLD can't be swapped!"
+        );
+        require!(
+            args.len() % 4 == 0,
+            "Invalid number of router swap arguments"
+        );
+        let payment_in = payment_for_current_task.unwrap_esdt();
+        let mut returned_payments_by_router = self.multi_pair_swap(payment_in, args);
+
+        require!(
+            !returned_payments_by_router.is_empty(),
+            "Router swap returned 0 payments"
+        );
+
+        let last_payment_index = returned_payments_by_router.len() - 1;
+        let payment_out = returned_payments_by_router.take(last_payment_index);
+        payments_to_return.append_vec(returned_payments_by_router);
+        EgldOrEsdtTokenPayment::from(payment_out)
+    }
+
+    fn send_resulted_payments(
+        &self,
+        dest_addr: ManagedAddress,
+        min_expected_token_out: EgldOrEsdtTokenPayment,
+        payment_for_current_task: EgldOrEsdtTokenPayment,
+        payments_to_return: &mut PaymentsVec<Self::Api>,
+    ) {
+        self.require_min_expected_token(&min_expected_token_out, &payment_for_current_task);
+        if payment_for_current_task.token_identifier.is_egld() {
             self.send()
-                .direct_egld(&caller, &payment_for_next_task.amount);
+                .direct_egld(&dest_addr, &payment_for_current_task.amount);
         } else {
             payments_to_return.push(EsdtTokenPayment::new(
-                payment_for_next_task.token_identifier.unwrap_esdt(),
-                payment_for_next_task.token_nonce,
-                payment_for_next_task.amount,
+                payment_for_current_task.token_identifier.unwrap_esdt(),
+                payment_for_current_task.token_nonce,
+                payment_for_current_task.amount,
             ));
         }
         if !payments_to_return.is_empty() {
-            self.send().direct_multi(&caller, &payments_to_return);
+            self.send().direct_multi(&dest_addr, payments_to_return);
         }
     }
 
