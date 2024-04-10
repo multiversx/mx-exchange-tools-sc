92bce4daefa49c8bc7473e20dd60b477dd6847a8
Dorin Marian Iancu <dorin.iancu@multiversx.com>
2024-04-10 11:16:30 +0300

    try create lp from farm pos test

--- auto-pos-creator/tests/negative_tests.rs
+++ auto-pos-creator/tests/negative_tests.rs
@@ -4,9 +4,13 @@ use auto_pos_creator::{
     external_sc_interactions::router_actions::SwapOperationType,
     multi_contract_interactions::create_pos_endpoints::CreatePosEndpointsModule,
 };
-use multiversx_sc::types::{BigUint, ManagedBuffer, MultiValueEncoded};
+use multiversx_sc::{
+    codec::Empty,
+    types::{BigUint, ManagedBuffer, MultiValueEncoded},
+};
 use multiversx_sc_scenario::{managed_address, managed_token_id, rust_biguint, DebugApi};
 use pos_creator_setup::{PosCreatorSetup, LP_TOKEN_IDS, TOKEN_IDS};
+use tests_common::farm_with_locked_rewards_setup::FARM_TOKEN_ID;
 
 pub mod metastaking_setup;
 pub mod pair_setup;
@@ -131,4 +135,65 @@ fn try_create_lp_pos_from_same_lp_token() {
             },
         )
         .assert_user_error("Invalid tokens");
+}
+
+#[test]
+fn try_create_lp_pos_from_farm_pos() {
+    let pos_creator_setup = PosCreatorSetup::new(
+        farm_with_locked_rewards::contract_obj,
+        energy_factory::contract_obj,
+        pair::contract_obj,
+        router::contract_obj,
+        farm_staking::contract_obj,
+        farm_staking_proxy::contract_obj,
+        auto_pos_creator::contract_obj,
+    );
+    let b_mock = pos_creator_setup.farm_setup.b_mock;
+
+    let user_addr = pos_creator_setup.farm_setup.first_user;
+    let user_farm_tokens = 142_857_142u64;
+    b_mock.borrow_mut().set_nft_balance(
+        &user_addr,
+        FARM_TOKEN_ID[0],
+        1,
+        &rust_biguint!(user_farm_tokens),
+        &Empty,
+    );
+
+    // user enter (B, C) pair with token A
+    let first_pair_addr = pos_creator_setup.pair_setups[0]
+        .pair_wrapper
+        .address_ref()
+        .clone();
+    let third_pair_addr = pos_creator_setup.pair_setups[2]
+        .pair_wrapper
+        .address_ref()
+        .clone();
+    b_mock
+        .borrow_mut()
+        .execute_esdt_transfer(
+            &user_addr,
+            &pos_creator_setup.pos_creator_wrapper,
+            FARM_TOKEN_ID[0],
+            1,
+            &rust_biguint!(user_farm_tokens),
+            |sc| {
+                let mut swap_operations = MultiValueEncoded::new();
+                let swap_operation: SwapOperationType<DebugApi> = (
+                    managed_address!(&first_pair_addr),
+                    ManagedBuffer::from(SWAP_TOKENS_FIXED_INPUT_FUNC_NAME),
+                    managed_token_id!(TOKEN_IDS[1]), // Want token B,
+                    BigUint::from(1u64),
+                )
+                    .into();
+                swap_operations.push(swap_operation);
+                let _ = sc.create_lp_pos_from_single_token(
+                    managed_address!(&third_pair_addr),
+                    1u32.into(),
+                    1u32.into(),
+                    swap_operations,
+                );
+            },
+        )
+        .assert_user_error("Only fungible ESDT accepted");
 }
