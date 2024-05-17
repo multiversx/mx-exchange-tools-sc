21751517654f037af34b54f58375476d66a10726
Costin Carabas <costin.carabas@gmail.com>
2024-05-17 12:08:42 +0300

    Create interactor for auto-pos-creator

---
+++ auto-pos-creator/interact/src/auto_pos_creator_config.rs
@@ -0,0 +1,25 @@
+use multiversx_sc_snippets::imports::Bech32Address;
+use serde::Deserialize;
+use std::io::Read;
+
+/// Config file
+const CONFIG_FILE: &str = "config.toml";
+
+/// Multisig Interact configuration
+#[derive(Debug, Deserialize)]
+pub struct Config {
+    pub gateway: String,
+    pub wegld_address: Bech32Address,
+    pub router_address: Bech32Address,
+    pub random_address: Bech32Address,
+}
+
+impl Config {
+    // Deserializes config from file
+    pub fn load_config() -> Self {
+        let mut file = std::fs::File::open(CONFIG_FILE).unwrap();
+        let mut content = String::new();
+        file.read_to_string(&mut content).unwrap();
+        toml::from_str(&content).unwrap()
+    }
+}
