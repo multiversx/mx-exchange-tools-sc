2044c097b7babfcf27a05bd1f1157628b4317524
Claudiu Lataretu <claudiu.lataretu@gmail.com>
2025-07-09 12:45:47 +0300

    WIP
    - remove metabonding interactions
    - upgrade rust sdk framework
    
    Signed-off-by: Claudiu Lataretu <claudiu.lataretu@gmail.com>

--- auto-farm/src/external_sc_interactions/multi_contract_interactions.rs
+++ auto-farm/src/external_sc_interactions/multi_contract_interactions.rs
@@ -2,11 +2,6 @@ multiversx_sc::imports!();
 
 use crate::common::{rewards_wrapper::RewardsWrapper, unique_payments::UniquePayments};
 
-use super::metabonding_actions::SingleMetabondingClaimArg;
-
-pub type ClaimAllArgType<M> =
-    MultiValue2<ManagedAddress<M>, ManagedVec<M, SingleMetabondingClaimArg<M>>>;
-
 #[multiversx_sc::module]
 pub trait MultiContractInteractionsModule:
     read_external_storage::ReadExternalStorageModule
@@ -16,7 +11,6 @@ pub trait MultiContractInteractionsModule:
     + crate::registration::RegistrationModule
     + crate::user_tokens::user_farm_tokens::UserFarmTokensModule
     + crate::external_sc_interactions::farm_actions::FarmActionsModule
-    + crate::external_sc_interactions::metabonding_actions::MetabondingActionsModule
     + crate::external_sc_interactions::fees_collector_actions::FeesCollectorActionsModule
     + crate::external_sc_interactions::locked_token_merging::LockedTokenMergingModule
     + crate::whitelists::metastaking_whitelist::MetastakingWhitelistModule
@@ -46,29 +40,19 @@ pub trait MultiContractInteractionsModule:
     /// Even if left empty, it still needs to be prepended by its length,
     /// in this case, 4 bytes of 0.
     #[endpoint(claimAllRewardsAndCompound)]
-    fn claim_all_rewards_and_compound(
-        &self,
-        claim_args: MultiValueEncoded<ClaimAllArgType<Self::Api>>,
-    ) {
+    fn claim_all_rewards_and_compound(&self, users: MultiValueEncoded<ManagedAddress<Self::Api>>) {
         self.require_caller_proxy_claim_address();
 
         let locked_token_id = self.get_locked_token_id();
-        for user_claim_args_pair in claim_args {
-            let (user, metabonding_claim_args) = user_claim_args_pair.into_tuple();
-            self.claim_all_single_user(&user, metabonding_claim_args, locked_token_id.clone());
+        for user in users {
+            self.claim_all_single_user(&user, locked_token_id.clone());
         }
     }
 
-    fn claim_all_single_user(
-        &self,
-        user: &ManagedAddress,
-        metabonding_claim_args: ManagedVec<SingleMetabondingClaimArg<Self::Api>>,
-        locked_token_id: TokenIdentifier,
-    ) {
+    fn claim_all_single_user(&self, user: &ManagedAddress, locked_token_id: TokenIdentifier) {
         let user_id = self.user_ids().get_id_non_zero(user);
         let mut rew_wrapper = RewardsWrapper::new(locked_token_id);
 
-        self.claim_metabonding_rewards(user, metabonding_claim_args, &mut rew_wrapper);
         self.claim_fees_collector_rewards(user, &mut rew_wrapper);
         self.claim_all_farm_rewards(user, user_id, &mut rew_wrapper);
         self.claim_all_metastaking_rewards(user, user_id, &mut rew_wrapper);
